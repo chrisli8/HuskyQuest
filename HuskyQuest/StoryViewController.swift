de4c4b195221db183df646a91e483bf1f8d6e9c0
studentuser <studentuser@studentusers-MacBook-Pro.local>
2017-06-01 20:36:38 -0700

    installed cocoapods

---
+++ HuskyQuest/StoryViewController.swift
@@ -0,0 +1,17 @@
+//
+//  StoryViewController.swift
+//  HuskyQuest
+//
+//  Created by studentuser on 6/1/17.
+//  Copyright © 2017 Chris Li. All rights reserved.
+//
+
+import UIKit
+
+class StoryViewController: UIViewController {
+
+    @IBOutlet weak var StoryTextBox: UITextView!
+    @IBOutlet weak var Choice1Button: UIButton!
+    @IBOutlet weak var Choice2Button: UIButton!
+    @IBOutlet weak var Choice3Button: UIButton!
+}
