931b3ff27b998234ae6db1f0eed80abcf1a3eb0e
chrisli8 <8chrisgli@gmail.com>
2017-06-05 15:47:48 -0700

    learn page functional with experience level

--- HuskyQuest/LearnViewController.swift
+++ HuskyQuest/LearnViewController.swift
@@ -19,15 +19,76 @@ class LearnViewController: UIViewController {
     @IBOutlet weak var diligenceProgress: UIProgressView!
     @IBOutlet weak var experienceLabel: UILabel!
 
+    var data = AppData.shared
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        experienceLabel.text = String(data.experience)
+        updateExperience()
+        updateProgress()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        
+    }
+    
+    // MARK: - Helper Methods
+    
+    // enables and diables stat-up buttons when experence is 0
+    // deincrements experience by one
+    func updateExperience() {
+        if data.experience <= 0 {
+            diligenceUpBtn.isHidden = true
+            creativityUpBtn.isHidden = true
+            understandingUpBtn.isHidden = true
+            charismaUpBtn.isHidden = true
+        } else {
+            data.experience = data.experience - 1
+            diligenceUpBtn.isHidden = false
+            creativityUpBtn.isHidden = false
+            understandingUpBtn.isHidden = false
+            charismaUpBtn.isHidden = false
+        }
+        experienceLabel.text = String(data.experience)
+    }
+    
+    // redraws progress bars based on
+    func updateProgress() {
+        // Set Stats
+        diligenceProgress.setProgress(Float(AppData.shared.stats["Diligence"]!) / 25.0, animated: false)
+        creativityProgress.setProgress(Float(AppData.shared.stats["Creativity"]!) / 25.0, animated: false)
+        understandingProgress.setProgress(Float(AppData.shared.stats["Understanding"]!) / 25.0, animated: false)
+        charismaProgress.setProgress(Float(AppData.shared.stats["Charisma"]!) / 25.0, animated: false)
+    }
+    
+    // MARK: - Actions
+    
+    @IBAction func diligenceUpPressed(_ sender: Any) {
+        updateExperience()
+        data.stats["Diligence"] = data.stats["Diligence"]! + 1
+        updateProgress()
+    }
+    
+    @IBAction func creativityUpPressed(_ sender: Any) {
+        updateExperience()
+        data.stats["Creativity"] = data.stats["Creativity"]! + 1
+        updateProgress()
+    }
+    
+    @IBAction func understandingUpPressed(_ sender: Any) {
+        updateExperience()
+        data.stats["Understanding"] = data.stats["Understanding"]! + 1
+        updateProgress()
+    }
+    
+    @IBAction func charismaUpPressed(_ sender: Any) {
+        updateExperience()
+        data.stats["Charisma"] = data.stats["Charisma"]! + 1
+        updateProgress()
     }
     
 
