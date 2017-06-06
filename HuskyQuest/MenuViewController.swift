1563c3cca716a3989f5181c54e9c9231d7a359b0
chrisli8 <8chrisgli@gmail.com>
2017-06-05 23:00:05 -0700

    code for notification in menu added to viewWillLoad to avoid visual confusion

--- HuskyQuest/MenuViewController.swift
+++ HuskyQuest/MenuViewController.swift
@@ -17,8 +17,8 @@ class MenuViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
         learnNotification.isHidden = true
         if AppData.shared.experience > 0 {
             learnNotification.isHidden = false
