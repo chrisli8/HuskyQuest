2af17c2879b62ecce27e41591bc0ead7d938558e
chrisli8 <8chrisgli@gmail.com>
2017-06-05 22:55:08 -0700

    notification icon added in menu view when there are experience points to add

---
+++ HuskyQuest/MenuViewController.swift
@@ -0,0 +1,47 @@
+//
+//  MenuViewController.swift
+//  HuskyQuest
+//
+//  Created by Chris Li on 6/5/17.
+//  Copyright © 2017 Chris Li. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+    @IBOutlet weak var learnNotification: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        learnNotification.isHidden = true
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        learnNotification.isHidden = true
+        if AppData.shared.experience > 0 {
+            learnNotification.isHidden = false
+        }
+        learnNotification.layer.cornerRadius = 15
+        learnNotification.layer.masksToBounds = true
+        learnNotification.text = String(Int(AppData.shared.experience as Double))
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
